7e846bdfe461addc6b8b205e7e41afd64a5fb203
Lateef <peleowold@gmail.com>
2026-04-25 18:52:05 +0100

    Added destination inside the bookingsSchema

--- Booking.js
+++ Booking.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const bookingSchema = new mongoose.Schema({
     passengerName: { type: String, required: true },
     pickupLocation: { type: String, required: true },
+    destination: { type: String, required: true },
     taxiId: { type: Number, required: true },
     status: { type: String, default: "Confirmed" },
     bookedAt: { type: Date, default: Date.now }
